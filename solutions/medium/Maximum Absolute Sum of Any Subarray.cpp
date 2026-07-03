933311ae38ca6c679ff96fd26cbaf11f516d0f27
Parth Gupta <guptaparth2209@gmail.com>
2026-07-03 20:25:17 +0530

    Add solution for Maximum Absolute Sum of Any Subarray

---
+++ solutions/medium/Maximum Absolute Sum of Any Subarray.cpp
@@ -0,0 +1,26 @@
+// Title: Maximum Absolute Sum of Any Subarray
+            // Difficulty: Medium
+            // Language: C++
+            // Link: https://leetcode.com/problems/maximum-absolute-sum-of-any-subarray/
+
+            int mod_worst = -worst;
+            best = max(v1,v2);
+            worst = min(v3,v2);
+            if( worst < 0){
+        for(i = 1; i< n ; i++){
+            int v1 = best + nums[i];
+            int v2 = nums[i];
+            int v3 = worst + nums[i];
+        if (n == 1){
+        int ans = nums[0];
+        int worst = nums[0];
+        int best = nums[0];
+        
+        int i = 0;
+        int n = nums.size();
+    int maxAbsoluteSum(vector<int>& nums) {
+public:
+            if (nums[0]< 0){
+        }
+                return -ans;
+            }
